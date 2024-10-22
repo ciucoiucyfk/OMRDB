2ca4914da1ab7ed684f9e5a2e3d4b016c9cbc5e9
TYRAXES <abhipnair2509@gmail.com>
2024-10-23 00:14:08 +0530

    update 5

--- bin/writer.py
+++
@@ -1,6 +0,0 @@
-import pickle
-ak = ['a','a','a','a','a','a','a','a','a','a']
-def write(dic):
-    file= open('C:/Users/Abhinav/Documents/Class 12 Project Computer/bin/config.dat','wb')
-    pickle.dump([dic,['master','a123','master']],file)
-write(ak)
